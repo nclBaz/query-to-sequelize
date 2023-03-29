48f7f21de05311d53bfc830d91930367f87b2afc
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-03-29 15:26:04 +0200

    Adds projection functions

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const querystring = require("querystring")
+const iso8601 = require("./lib/iso8601-regex")
+
+// Convert comma separated list to a sequelize projection (attributes array).
+// for example f('field1,field2,field3') -> ['field1','field2','field3']
+function fieldsToAttributes(fields) {
+  if (!fields) return null
+  return fields.split(",")
+}
+
+// Convert comma separated list to a sequelize projection (attributes object)
+// for example f('field2') -> {field2:false}
+function omitFieldsToAttributes(omitFields) {
+  if (!omitFields) return null
+  return { exclude: omitFields.split(",") }
+}
+
 module.exports = function (query, options) {
   return { criteria: {}, options: {}, links: function (url, totalCount) {} }
 }
