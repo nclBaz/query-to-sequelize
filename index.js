a0208426d615c353136efccb919f412241488671
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-03-29 15:37:03 +0200

    Adds sortToSequelize

--- index.js
+++ index.js
@@ -3,16 +3,28 @@ const iso8601 = require("./lib/iso8601-regex")
 
 // Convert comma separated list to a sequelize projection (attributes array).
 // for example f('field1,field2,field3') -> ['field1','field2','field3']
-function fieldsToAttributes(fields) {
+function fieldsToSequelize(fields) {
   if (!fields) return null
   return fields.split(",")
 }
 
-// Convert comma separated list to a sequelize projection (attributes object)
-// for example f('field2') -> {field2:false}
-function omitFieldsToAttributes(omitFields) {
+// Convert comma separated list to a sequelize projection (attributes object).
+// for example f('field1,field2,field3') -> { exclude: ['field1','field2','field3']}
+function omitFieldsToSequelize(omitFields) {
   if (!omitFields) return null
   return { exclude: omitFields.split(",") }
+}
+
+// Convert comma separated list to sequelize sort options (sort array).
+// for example f('field1,+field2,-field3') -> [['field1', 'ASC'], ['field2', 'ASC'], ['field3', 'DESC']]
+function sortToSequelize(sort) {
+  const sortArray = []
+  if (!sort) return sortArray
+  sort.split(",").forEach(function (field) {
+    const c = field.charAt(0) === "-"
+    sortArray.push([c ? field.substring(1) : field, c ? "DESC" : "ASC"])
+  })
+  return sortArray
 }
 
 module.exports = function (query, options) {
